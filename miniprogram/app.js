e4287e201ad86a854d13b878effae1c024526bf2
FrzMtrsprt <alex20020703@163.com>
2022-11-07 12:33:31 +0800

    Get open id on launch

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,30 @@
 App({
-    onLaunch:function(){
+    /**
+     * 小程序全局变量
+     */ 
+    global: {
+        openid: '',
+        loginStatus: ''
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLaunch: function () {
+        // 初始化云开发
         wx.cloud.init({
-            env:"cloud1-3g52quh891d8cf4e"
+            env: "cloud1-3g52quh891d8cf4e"
         })
-    }
+
+        // 获取 open id
+        wx.cloud.callFunction({
+            name: 'quickstartFunctions',
+            data: { type: 'getOpenId' }
+        }).then(res => {
+            this.global.openid = res.result.openid
+        }).catch(err => {
+            console.log(err)
+        })
+    },
+
 })
