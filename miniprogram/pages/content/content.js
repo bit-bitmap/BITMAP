dd93d5fb62c3d8bacddcaf54f762df37535c1b73
Ymir612 <cathyymir@outlook.com>
2022-11-13 16:08:09 +0800

    content

--- miniprogram/pages/content/content.js
+++ miniprogram/pages/content/content.js
@@ -7,168 +7,19 @@ Page({
      * 页面的初始数据
      */
     data:{
+        detail:'',
         imgurl1:"../../images/shoucang-no.png",
         imgurl2:"../../images/like-no.png",
-        datalist:[],
-
-        cateItems: [
+        Author:[
             {
-              cate_id: 1,
-              cate_name: "学习",
-              ishaveChild: true,
-              children:
-              [
-                {
-                  child_id: 1,
-                  name: '选课',
-                },
-                {
-                  child_id: 2,
-                  name: '作业',
-                },
-                {
-                  child_id: 3,
-                  name: '考试',
-                },
-                {
-                  child_id: 4,
-                  name: '保研',
-                },
-                {
-                    child_id: 5,
-                    name: '考研',
-                  },
-                {
-                  child_id: 6,
-                  name: '留学',
-                },
-                {
-                    child_id: 7,
-                    name: '辅导/组队',
-                  }
-              ]
+                author_id:1,
+                author_name:"作者A"
             },
             {
-                cate_id: 2,
-                cate_name: "生活",
-                ishaveChild: true,
-                children:
-                [
-                  {
-                    child_id: 1,
-                    name: '趣事',
-                  },
-                  {
-                    child_id: 2,
-                    name: '捞人/群',
-                  },
-                  {
-                    child_id: 3,
-                    name: '提醒/吐槽',
-                  },
-                  {
-                    child_id: 4,
-                    name: '表白/征婚',
-                  },
-                  {
-                    child_id: 5,
-                    name: '失物',
-                  },
-                  {
-                    child_id: 6,
-                    name: '推荐',
-                  },
-                  {
-                    child_id: 7,
-                    name: '假期',
-                  }
-                ]
-              },
-            {
-              cate_id: 3,
-              cate_name: "运动",
-              ishaveChild: true,
-              children:
-              [
-                {
-                  child_id: 1,
-                  name: '比赛',
-                },
-                {
-                  child_id: 2,
-                  name: '田径',
-                },
-                {
-                  child_id: 3,
-                  name: '球类',
-                },
-                {
-                  child_id: 4,
-                  name: '场地',
-                },
-                {
-                    child_id: 5,
-                    name: '健康',
-                  },
-                  {
-                    child_id: 6,
-                    name: '课程',
-                  },
-                  {
-                    child_id: 7,
-                    name: '其他',
-                  }
-              ]
+                author_id:2,
+                author_name:"作者B"
             },
-            {
-              cate_id: 4,
-              cate_name: "美食",
-              ishaveChild: true,
-              children:
-              [
-                {
-                  child_id: 1,
-                  name: '美食分享',
-                },
-                {
-                  child_id: 2,
-                  name: '外卖推荐',
-                },
-                {
-                  child_id: 3,
-                  name: '踩坑避雷',
-                },
-                {
-                  child_id: 4,
-                  name: '养生专区',
-                }
-              ]
-            },
-            {
-                cate_id: 5,
-                cate_name: "活动",
-                ishaveChild: true,
-                children:
-                [
-                  {
-                    child_id: 1,
-                    name: '志愿者服务',
-                  },
-                  {
-                    child_id: 2,
-                    name: '社团活动',
-                  },
-                  {
-                    child_id: 3,
-                    name: '假期出行',
-                  },
-                  {
-                    child_id: 4,
-                    name: '校园活动',
-                  }
-                ]
-              }
-          ]
+        ]
 
     },
 
@@ -204,17 +55,18 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad() {
-        wx.cloud.database().collection('contentDetail')
+    onLoad(options) {
+        console.log("接收的id",options.id)
+        wx.cloud.database().collection("articlelist").doc(options.id)
         .get()
-        .then(res =>{
-            console.log("获取成功",res)
+        .then(res=>{
+            console.log("接收成功",res)
             this.setData({
-                datalist:res.data
+                detail:res.data
             })
         })
-        .catch(res =>{
-            console.log("获取失败",res)
+        .catch(res=>{
+            console.log("接收失败",res)
         })
     },
 
