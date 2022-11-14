41b75c8397eac6d4acc79dfe321f5e6dd51b5fc5
leyan258 <1913790861@qq.com>
2022-11-14 16:19:00 +0800

    info9

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -7,6 +7,9 @@ Page({
      * 页面的初始数据
      */
     data: {
+        isEndOfList: false,
+        pagesize: 3,
+        pagenum : 1,
         info: "",
         datalist:[
             
@@ -19,18 +22,6 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad() {
-        wx.cloud.database().collection("articlelist").where({flag:true})
-        .get()
-        .then(res=>{
-            console.log("获取成功",res)
-            this.setData({
-                datalist:res.data
-            })
-        })
-        .catch(res=>{
-            console.log("获取失败",res)
-        })
-
         wx.cloud.database().collection("articlelist").where({guanfang:true})
         .get()
         .then(res=>{
@@ -42,7 +33,33 @@ Page({
         .catch(res=>{
             console.log("获取失败",res)
         })
+        wx.cloud.database().collection("articlelist")
+        .where({flag:true})
+        .limit(this.data.pagesize)
+        .get()
+        .then(res=>{
+            console.log("获取成功l ",res)
+            this.setData({
+                datalist:res.data
+            })
+        })
     },
+
+
+    // getData: function(num) {
+    //     wx.cloud.database().collection("articlelist")
+    //       .where({flag:true})
+    //       .skip(num?num:this.data.list.length)
+    //       .limit(this.data.limit)
+    //       .get()
+    //       .then(res => {
+    //         this.setData({
+    //           datalist: [...this.data.datalist, ...res.data], //合并数据
+    //           isEndOfList: res.data.length < this.data.limit ? true : false //判断是否结束
+    //         })
+    //       })
+    //   },
+
 
     /**
      * 跳转到详情页
@@ -85,15 +102,78 @@ Page({
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh() {
+    onRefresh:function(){
+      //导航条加载动画
+      wx.showNavigationBarLoading()
+      //loading 提示框
+      wx.showLoading({
+        title: '别刷了没有了',
+      })
+      console.log("下拉刷新啦");
+      setTimeout(function () {
+        wx.hideLoading();
+        wx.hideNavigationBarLoading();
+        //停止下拉刷新
+        wx.stopPullDownRefresh();
+      }, 2000)
+    },
+    
+    topRefresh:function(
+        pagenum=this.data.pagenum,
+        pagesize=this.data.pagesize
+    ) {
+        wx.cloud.database().collection("articlelist")
+        .where({flag:true})
+        .limit(this.data.pagesize)
+        .skip(pagenum*pagesize)
+        .get()
+        .then(res => {
+            console.log("获取成功上 ",res)    
+            this.setData({
+                  datalist:res.data, 
+                  pagenum: pagenum+1,
+                  isEndOfList: res.data.length < this.data.pagesize ? true : false
+                })
+              })
+    },
 
+    bottomRefresh:function(
+        pagenum=this.data.pagenum,
+        pagesize=this.data.pagesize
+    ) {
+        wx.cloud.database().collection("articlelist")
+        .where({flag:true})
+        .limit(this.data.pagesize)
+        .skip(pagenum*pagesize)
+        .get()
+        .then(res => {
+                this.setData({
+                  datalist: [...this.data.datalist, ...res.data], 
+                  pagenum: pagenum+1,
+                  isEndOfList: res.data.length < this.data.pagesize ? true : false 
+                })
+              })
+    },
+
+    onPullDownRefresh:function() {
+        
+        if (this.data.isEndOfList== true ) {
+            this.onRefresh()
+        } else {
+            this.topRefresh()
+        }
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
+    
     onReachBottom() {
-
+        if (this.data.isEndOfList== true ) {
+            this.onRefresh()
+        } else {
+            this.bottomRefresh()
+        }
     },
 
     /**
