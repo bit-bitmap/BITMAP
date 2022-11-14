26df71560c40cb30092d40dbc21117a3088ee043
Kong Ziyue <kzy18203606191@163.com>
2022-11-14 17:02:05 +0800

    upload8

--- miniprogram/pages/upload/upload.js
+++ miniprogram/pages/upload/upload.js
@@ -1,6 +1,10 @@
 // pages/upload/upload.js
-let upbase=[]
+const app = getApp()
+let upbase=[]  //上传数据库的图片
+let upbasetitle=""  //标题
+let upbasetext=""  //内容
 let db=wx.cloud.database()
+
 Page({
 
     /**
@@ -13,58 +17,97 @@ Page({
       titleplaceholder:"好的标题让别人更容易关注你~",
       textplaceholder:"至少5个字嗷~",
       Imagespath:[],
-      src:[]
+      choosecate: 0,
+    //   image:[],  //云数据库中articlelist存储的图片网址
+      isNineImages:true,  //是否显示上传"+"按键
+      isSubmit:false,  //article是否发表
+      isAccess:false  //article是否通过审核
     },
 
-//标题字数动态监测
-inputNum: function (e) {
+//标题字数动态监测，实时获取标题文本
+inputtitleNum: function (e) {
     var value=e.detail.value;
     var length=parseInt(value.length);
+    upbasetitle = value;
     this.setData({
         titleNum: length
     });
 },
+//实时获取内容文本
+inputtext(e){
+    var value=e.detail.value;
+    upbasetext = value;
+},
 
-//点击上传图片
-// handleChooseImages: function() {
-//   wx.chooseImage({
-//     // 同时选中的图片的数量
-//     count: 9,
-//     // 图片的格式  原图  压缩
-//     sizeType: ['original', 'compressed'],
-//     // 图片的来源  相册  照相机
-//     sourceType: ['album', 'camera'],
-//     success: (result) => {
-//       this.setData({
-//         // 图片数组 进行拼接 
-//         Imagespath: [...this.data.Imagespath, ...result.tempFilePaths]
-//       })
-//     }
-//   })
-// },
+handleFormSubmit(e){
+    db.collection("articlelist").add({
+        data:{
+          title: upbasetitle,
+          detail: upbasetext
+        }
+      }).then(res=>{
+        wx.showToast({
+          title: '上传成功',
+        })
+      }).catch(error => {
+        // handle error
+        console.log(error)
+      })
+},
 
-// handleRemoveImages: function(e){
-//     // 2 获取被点击的组件的索引
-//     const { index } = e.currentTarget.dataset;
-//     // 3 获取data中的图片数组
-//     let { Imagespath } = this.data;
-//     // 4 删除元素
-//     Imagespath.splice(index, 1);
-//     this.setData({
-//       Imagespath
-//     })
-// },
+handleRemoveImages: function(e){
+    //获取被点击的组件的索引
+    const index = e.currentTarget.dataset;
+    //获取data中的图片数组
+    let arr= this.data.Imagespath;
+    //删除元素
+    arr.splice(index, 1);
+    this.setData({
+      Imagespath:arr
+    })
+},
 
+//将图片上传并显示
+handleUpImages(){
+    let that=this
+    wx.chooseImage({
+      // 同时选中的图片的数量
+      count: 9,
+      // 图片的格式  原图  压缩（手机）
+      sizeType: ['original', 'compressed'],
+      // 图片的来源  相册  照相机（手机）
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        console.log(result.tempFilePaths)
+        that.setData({
+          //将图片显示于界面
+          //将上传的图片地址赋值给Imagespath
+          Imagespath: result.tempFilePaths
+        })
+      //   wx.showLoading({
+      //     title: '上传中',
+      //   })
+      }
+    })
+  },
 //预览图片 全屏显示
-viewimage(){
+viewimage(e){
   let that=this
+  //获取所点击图片序号
+  let index=e.currentTarget.dataset.index
+  let item=that.data.Imagespath[index]
+  console.log(index)
+  //显示所点击图片
   wx.previewImage({
+    current: item,
     urls: that.data.Imagespath ,//需要预览图片的http链接列表
   })
 },
-//将图片上传到云数据库
-handleUpImagesCloud(){
+
+//将图片上传到云存储、云数据库
+handleFormSubmitImages(){
   let that=this
+  //云存储图片
   wx.showLoading({
     title: '上传中',
   })
@@ -87,145 +130,95 @@ handleUpImagesCloud(){
       console.log(error)
     })
   }
+  //云数据库
+  that.upbase()
 },
-
+//图片上传至云数据库
 upbase(){
   console.log("upbase的值",upbase)
-  db.collection("baseimage_test").add({
+  db.collection("articlelist").add({
     data:{
-      src:upbase
+      image:upbase
     }
   }).then(res=>{
     wx.showToast({
       title: '上传成功',
     })
-  })
-},
-//显示数据库中的图片
-basemessage(){
-  let that=this
-  db.collection("baseimage_test").get()
-  .then(res=>{
-    // console.log(res)
-    that.setData({
-      src:res.data
-    })
+  }).catch(error => {
+    // handle error
+    console.log(error)
   })
 },
 
-handleUpImages(){
-  let that=this
-  wx.chooseImage({
-    // 同时选中的图片的数量
-    count: 9,
-    // 图片的格式  原图  压缩（手机）
-    sizeType: ['original', 'compressed'],
-    // 图片的来源  相册  照相机（手机）
-    sourceType: ['album', 'camera'],
-    success: (result) => {
-      console.log(result.tempFilePaths)
-      that.setData({
-        //将图片显示于界面
-        //将上传的图片地址赋值给Imagespath
-        Imagespath: result.tempFilePaths
-      })
-    }
-  })
-},
+//显示数据库中的图片
+// basemessage(){
+//   let that=this
+//   db.collection("baseimage_test").get()
+//   .then(res=>{
+//     // console.log(res)
+//     that.setData({
+//       src:res.data
+//     })
+//   })
+// },
 
 // 文本域的输入的事件
-handleTextInput(e) {
-  this.setData({
-    textwritten: e.detail.value
-  })
-},
+// handleTextInput(e) {
+//   this.setData({
+//     textwritten: e.detail.value
+//   })
+// },
 // 提交按钮的点击
-handleFormSubmit() {
-  // 1 获取文本域的内容 图片数组
-  const { textwritten, Imagespath } = this.data;
-  // 2 合法性的验证
-  if (!textwritten.trim()) {
-    // 不合法
-    wx.showToast({
-      title: '输入不合法',
-      icon: 'none',
-      mask: true
-    });
-    return;
-  }
+// handleFormSubmit() {
+//   // 1 获取文本域的内容 图片数组
+//   const { textwritten, Imagespath } = this.data;
+//   // 2 合法性的验证
+//   if (!textwritten.trim()) {
+//     // 不合法
+//     wx.showToast({
+//       title: '输入不合法',
+//       icon: 'none',
+//       mask: true
+//     });
+//     return;
+//   }
 
-  // 准备上传图片 到专门的图片服务器 
-  // 上传文件的 api 不支持 多个文件同时上传  遍历数组 挨个上传 
-  // 显示正在等待的图片
-  wx.showLoading({
-    title: "正在上传中",
-    mask: true
-  });
+//   // 判断有没有需要上传的图片数组
+//   if (chooseImgs.length != 0) {
+//     chooseImgs.forEach((v, i) => {
+//       wx.uploadFile({
+//         // 图片要上传到哪里
+//         url: 'https://images.ac.cn/Home/Index/UploadAction/',
+//         // 被上传的文件的路径
+//         filePath: v,
+//         // 上传的文件的名称 后台来获取文件  file
+//         name: "file",
+//         // 顺带的文本信息
+//         formData: {},
+//         success: (result) => {
+//           console.log(result);
+//           let url = JSON.parse(result.data).url;
+//           this.UpLoadImgs.push(url);
 
-  // 判断有没有需要上传的图片数组
-  if (chooseImgs.length != 0) {
-    chooseImgs.forEach((v, i) => {
-      wx.uploadFile({
-        // 图片要上传到哪里
-        url: 'https://images.ac.cn/Home/Index/UploadAction/',
-        // 被上传的文件的路径
-        filePath: v,
-        // 上传的文件的名称 后台来获取文件  file
-        name: "file",
-        // 顺带的文本信息
-        formData: {},
-        success: (result) => {
-          console.log(result);
-          let url = JSON.parse(result.data).url;
-          this.UpLoadImgs.push(url);
+//           // 所有的图片都上传完毕了才触发  
+//           if (i === chooseImgs.length - 1) {
 
-          // 所有的图片都上传完毕了才触发  
-          if (i === chooseImgs.length - 1) {
+//             wx.hideLoading();
 
-            wx.hideLoading();
-
-
-            console.log("把文本的内容和外网的图片数组 提交到后台中");
-            //  提交都成功了
-            // 重置页面
-            this.setData({
-              textwritten: "",
-              chooseImgs: []
-            })
-            // 返回上一个页面
-            wx.navigateBack({
-              delta: 1
-            });
-
-          }
-        }
-      });
-    })
-  }else{
-    wx.hideLoading();
+//           }
+//         }
+//       });
+//     })
+//   }else{
+//     wx.hideLoading();
       
-    console.log("只是提交了文本");
-    wx.navigateBack({
-      delta: 1
-    });
+//     console.log("只是提交了文本");
+//     wx.navigateBack({
+//       delta: 1
+//     });
       
-  }
-},
-
- /**监听编辑器初始化完成时触发 */
-//  onBindReadyEditor(e) {
-//   console.log('onBindReadyEditor---------------->', e.detail)
-//   e.detail.setContents('666')
+//   }
 // },
-
-/**编辑器内容改变时触发 */
-// onBindInputEditor(e) {
-//   console.log("onBindInputEditor--------------------------->", e.detail)   
-// },
- /**编辑器失去焦点 */
-//  onBindBlurEditor(e) {
-//   console.log("onBindBlurEditor--------------------------->", e.detail)
-//  },
 
 //选择分类
 gotoCategory: function() {
@@ -240,8 +233,16 @@ gotoCategory: function() {
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad(options) {
-
+    onLoad() {
+        // 获取 open id
+        wx.cloud.callFunction({
+            name: "quickstartFunctions",
+            data: { type: "getOpenId" }
+        }).then(res => {
+            app.global.openid = res.result.openid
+        }).catch(err => {
+            console.log("openid",err)
+        })
     },
 
     /**
